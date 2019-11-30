aea618ee50a21d91462fb958f99aa0ee333a811e
Stelele <giftmugweni@gmail.com>
2019-11-30 15:44:07 +0200

    added price showing and checkout modal after finished

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,10 +1,16 @@
-function chosenColor(color,id){
+function chosenColor(color,id,price){
+    //Record the chosen color and display said color and price on the page 
     document.getElementById("disColor").innerHTML = color;
     document.getElementById("chosenId").value = id;
     document.getElementById("colorName").innerHTML = color;
+
+    //desplay unit price and store value
+    document.getElementById("chosenPriceDisplay").innerHTML = "$" + price
+    document.getElementById("chosenPrice").value = price
 }
 
 function updateNum(num){
+    //increment and decrement color selector amount
     var sorceValue = document.getElementById('itemNum');
     var currentValue = parseInt(sorceValue.innerHTML,10);
 
@@ -18,11 +24,14 @@ function updateNum(num){
 }
 
 function addCart(){
-    var currentValue = parseInt(document.getElementById('itemNum').innerHTML,10);
+    //function that displays selected color n times and calculates the subsequent total cost
+    var colorNum = parseInt(document.getElementById('itemNum').innerHTML,10);
     var chosenId = document.getElementById('chosenId').value;
+    var chosenPrice = parseFloat(document.getElementById("chosenPrice").value);
+    var totalCost = parseFloat(document.getElementById("totalPriceValue").value);
     
     var i
-    for(i = 0; i < currentValue; i++){
+    for(i = 0; i < colorNum; i++){
         var lastValue = document.getElementById("lastValue")
         var newID = parseInt(lastValue.value,10) + 1;
         
@@ -33,14 +42,90 @@ function addCart(){
 
         document.getElementById('details').append(newButton);
         document.getElementById(newButton.id).style.padding = "2%";
+
+        totalCost += chosenPrice;
         
     }
 
-    var totalColor = document.getElementById('checkoutNum')
-    totalColor.innerHTML = parseInt(totalColor.innerHTML,10) + currentValue
+    var totalColor = document.getElementById('checkoutNum');
+    totalColor.innerHTML = parseInt(totalColor.innerHTML,10) + colorNum;
+
+    document.getElementById("totalPriceValue").value = totalCost;
+    document.getElementById("totalPriceDisplay").innerHTML = "$" + totalCost.toFixed(2);
 
     document.getElementById("itemNum").innerHTML = 0;
     document.getElementById("closeModal").click();
-    document.getElementById("cart").innerHTML = "Checkout Now"
+    document.getElementById("checkOut").hidden = false;
 
+}
+
+function displayCartDetails(){
+    //function that shows a summary of all the colors selected and the total cost
+    let checkOutBody = document.getElementById("checkOutBody")
+    let detailsBody = document.getElementById("details").getElementsByTagName("button")
+    let totalCost = document.getElementById("totalPriceDisplay").innerHTML
+    let chosenColors = new Object();
+
+    //get dictionary of the occurances of each color
+    for(var i = 0; i < detailsBody.length; i++){
+        let color = detailsBody[i].name;
+
+        if(color in chosenColors){
+            chosenColors[color] += 1;
+        }else{
+            chosenColors[color] = 1;
+        }
+    }
+
+    //display summary in nodal
+    let headingRow = document.createElement("div");
+    headingRow.className = "row";
+    
+    let columnOne = document.createElement("div");
+    columnOne.className = "col";
+    columnOne.innerHTML = "<h3>Color</h3>";
+
+    let columTwo = document.createElement("div");
+    columTwo.className = "col";
+    columTwo.innerHTML = "<h3>Quantity</h3>";
+
+    headingRow.appendChild(columnOne);
+    headingRow.appendChild(columTwo);
+    checkOutBody.appendChild(headingRow);
+
+    for(color in chosenColors){
+        let row = document.createElement("div");
+        row.className = "row";
+
+        let column = document.createElement("div");
+        column.className = "col";
+        column.innerHTML = "<h4>"+ color +"</h4>";
+
+        row.appendChild(column);
+
+        column = document.createElement("div");
+        column.className = "col";
+        column.innerHTML = "<h4>"+ chosenColors[color] +"</h4>";
+
+        row.appendChild(column);
+
+        checkOutBody.appendChild(row)
+
+    }
+
+    let totalRow = document.createElement("div")
+    totalRow.className = "row"
+    
+    columnOne = document.createElement("div")
+    columnOne.className = "col"
+    columnOne.innerHTML = "<h4><strong>Total Cost</strong></h4>"
+
+    columTwo = document.createElement("div");
+    columTwo.className = "col";
+    columTwo.innerHTML = "<h4>" + totalCost + "</h4";
+
+    totalRow.appendChild(columnOne);
+    totalRow.appendChild(columTwo);
+
+    checkOutBody.appendChild(totalRow);
 }
